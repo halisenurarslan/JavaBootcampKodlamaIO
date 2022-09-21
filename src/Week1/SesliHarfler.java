99945118b137d6191e3988bdbb965efdcbbdd771
salih <salih@SALIH>
2022-09-21 23:38:44 +0300

    h

--- src/Week1/SesliHarfler.java
+++ src/Week1/SesliHarfler.java
@@ -7,19 +7,19 @@ public class SesliHarfler {
 
 		switch (harf) {
 		case 'A', 'I', 'O', 'U':
-			System.out.println("Kalın - Buyuk sesli harf");
+			System.out.println("Kalın - Büyük sesli harf");
 			break;
 		case 'a', 'ı', 'o', 'u':
-			System.out.println("Kalın - Kucuk sesli harf");
+			System.out.println("Kalın - Küçük sesli harf");
 			break;
 		case 'E', 'İ', 'Ö', 'Ü':
-			System.out.println("Ince - Buyuk sesli harf");
+			System.out.println("İnce - Büyük sesli harf");
 			break;
 		case 'e', 'i', 'ö', 'ü':
-			System.out.println("Ince - Kucuk sesli harf");
+			System.out.println("İnce - Küçük sesli harf");
 			break;
 		default:
-			System.out.println("Girilen harf sessiz harftir");
+			System.out.println("Girilen harf sessiz harftir...");
 		}
 
 	}
